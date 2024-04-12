ac98feee0b8eea454abbe23a9a1c5d33783b87a7
Peter <pwhelanstudio@gmail.com>
2024-04-12 17:30:41 +0100

    Add: Theme functions, TopBar start

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,3 +1,13 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColourModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOurlinedIcon from "@mui/icons-material/LightModeOutlined";
+import LightModeOurlinedIcon from "@mui/icons-material/LightModeOutlined";
+import LightModeOurlinedIcon from "@mui/icons-material/LightModeOutlined";
+import LightModeOurlinedIcon from "@mui/icons-material/LightModeOutlined";
+import LightModeOurlinedIcon from "@mui/icons-material/LightModeOutlined";
+import LightModeOurlinedIcon from "@mui/icons-material/LightModeOutlined";
 
 
 const Topbar = () => {
