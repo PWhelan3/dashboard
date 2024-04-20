884d5e3eb303cb78e0e366f6aa98cc81af440608
Peter <pwhelanstudio@gmail.com>
2024-04-21 00:22:47 +0100

    Add: Starting sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,4 +1,6 @@
-
+import { useState } from "react";
+import { ProSideBar , Menu, MenuItems } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
 
 const Sidebar = () => {
     return <div>Sidebar</div>
