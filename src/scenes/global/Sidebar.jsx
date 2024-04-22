39e501271cdce6f093c29135fe94d4a5f695de1a
Peter <pwhelanstudio@gmail.com>
2024-04-22 19:59:49 +0100

    Add: Sidebar Variables

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -22,7 +22,8 @@ import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 const Sidebar = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
-
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState("Dashboard");
 
     return <div>Sidebar</div>
 };
